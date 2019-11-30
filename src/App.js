d8ebfd31f02f2f9f00fed40ac9a377ec0966455f
Crys <sovereigncrys88@gmail.com>
2019-11-30 18:23:36 -0500

    fail update

---
+++ src/App.js
@@ -0,0 +1,70 @@
+
+import React, {Fragment} from 'react';
+import './App.css';
+import Navbar from './Navbar';
+import {Switch, Route} from 'react-router-dom';
+import ScrollToTop from './ScrollToTop';
+import About from './About';
+import Home from './Home';
+import Contact from './Contact';
+import Menu from './Menu';
+import Hours from './Hours';
+import Events from './Events';
+import Reservations from './Reservations';
+import Privacy from './Privacy';
+
+
+class App extends React.Component {
+  render(){
+  return (
+    <div>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+   <a 
+   style={styles.nav}                                             
+   href="https://www.google.com/search?q=is+it+illegal+to+not+pay+employees+in+virginia&oq=is+it+illegal+to+not+pay+employees+in+virginia&aqs=chrome..69i57.12488j0j7&sourceid=chrome&ie=UTF-8"> Chef T<br/>Do The Right Thing & Pay What You Owe! </a>
+   </div>
+   // <Fragment>
+    //       <div style={styles.nav}>
+    //           <Navbar/>
+    //       </div> 
+    //         <ScrollToTop>    
+    //             <Switch>
+    //               <Route exact path='/'  component={Home}/>
+    //               <Route exact path='/home'  component={Home}/>
+    //               <Route exact path='/menu'  component={Menu}/>
+    //               <Route exact path='/info'  component={Hours}/>
+    //               <Route exact path='/about'  component={About}/>
+    //               <Route exact path='/contact'  component={Contact}/>
+    //               <Route exact path='/events'  component={Events}/>
+    //               <Route exaxct path='/reservations' component={Reservations}/>
+    //               <Route exact path='/privacy'  component={Privacy}/>
+    //             </Switch>
+    //         </ScrollToTop>
+    // </Fragment>
+   );
+  }
+}
+
+export default App;
+
+const styles = {
+    nav: {
+      // position: 'fixed',
+      zIndex: '99',
+      width: '100%'
+    }
+
+}
+
+ 
